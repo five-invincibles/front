0170767489e455fe2fe52ec9521c4d51e113d877
을준 손 <son@sonserver.xyz>
2022-06-22 22:06:17 +0900

    make search and floataction

--- lib/Controller/cat_map_page_controller.dart
+++ lib/Controller/cat_map_page_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -7,22 +8,16 @@ import 'package:location/location.dart';
 
 class CatMapPageController extends GetxController {
   final Completer<GoogleMapController> mapController = Completer();
-  LocationData? currentLocation;
+  final TextEditingController searchTextController = TextEditingController();
+  Rx<LocationData>? currentLocation;
   RxBool isLoading = true.obs;
 
-  final CameraPosition kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  final CameraPosition kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
   Future _getCurrentLocation() async {
-    currentLocation = await Location().getLocation();
+    var location = Location();
+    currentLocation = (await Location().getLocation()).obs;
+    location.onLocationChanged.listen((loc) {
+      currentLocation!(loc);
+    });
   }
 
   @override
--- lib/View/cat_map_page.dart
+++ lib/View/cat_map_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:front/Controller/cat_map_page_controller.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -9,6 +11,7 @@ class CatMapPage extends GetView<CatMapPageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: _buildFloatingButton(),
       body: _buildBody(),
     );
   }
@@ -18,19 +21,88 @@ class CatMapPage extends GetView<CatMapPageController> {
         ? Center(
             child: Text('Loading'),
           )
-        : _buildMap());
+        : Stack(
+            children: [
+              _buildMap(),
+              _buildSearch(),
+            ],
+          ));
   }
 
   Widget _buildMap() {
-    return GoogleMap(
-      mapType: MapType.terrain,
-      initialCameraPosition: CameraPosition(
-          target: LatLng(controller.currentLocation!.latitude!,
-              controller.currentLocation!.longitude!),
-          zoom: 25),
-      onMapCreated: (GoogleMapController controller) {
-        this.controller.mapController.complete(controller);
-      },
+    return Obx(() => GoogleMap(
+          zoomControlsEnabled: false,
+          mapType: MapType.terrain,
+          initialCameraPosition: CameraPosition(
+              target: LatLng(controller.currentLocation!.value.latitude!,
+                  controller.currentLocation!.value.longitude!),
+              zoom: 17),
+          onMapCreated: (GoogleMapController controller) {
+            this.controller.mapController.complete(controller);
+          },
+          markers: {
+            Marker(
+                markerId: const MarkerId('current'),
+                position: LatLng(controller.currentLocation!.value.latitude!,
+                    controller.currentLocation!.value.longitude!)),
+          },
+        ));
+  }
+
+  Widget _buildSearch() {
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.black38),
+            borderRadius: BorderRadius.circular(30)),
+        margin:
+            EdgeInsets.symmetric(vertical: Get.height * 0.05, horizontal: 13),
+        height: 44,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          child: Row(children: [
+            Flexible(
+              child: IconButton(
+                padding: EdgeInsets.zero,
+                icon: Icon(Icons.menu),
+                onPressed: () {},
+              ),
+            ),
+            Flexible(
+              flex: 7,
+              child: TextField(
+                controller: controller.searchTextController,
+                decoration: InputDecoration(
+                    hintText: "우리 고양이는 어디에 있을까?",
+                    hintStyle: TextStyle(color: Colors.black.withOpacity(0.25)),
+                    border: InputBorder.none),
+              ),
+            ),
+            Flexible(
+              child: IconButton(
+                padding: EdgeInsets.zero,
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFloatingButton() {
+    return FloatingActionButton(
+      elevation: 0,
+      highlightElevation: 0,
+      focusElevation: 0,
+      hoverElevation: 0,
+      splashColor: Colors.white.withOpacity(0.1),
+      hoverColor: Colors.white,
+      backgroundColor: Color(0xFFFEBB6C),
+      child: const Icon(CupertinoIcons.plus),
+      onPressed: () {},
     );
   }
 }
