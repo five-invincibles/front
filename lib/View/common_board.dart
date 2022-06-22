37597403702053203ffacee26f07ba65d77bb0b4
을준 손 <son@sonserver.xyz>
2022-06-22 22:52:58 +0900

    make common board floatAction

--- lib/View/common_board.dart
+++ lib/View/common_board.dart
@@ -10,12 +10,23 @@ class CommonBoard extends GetView<CommonBoardController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        elevation: 5,
+        focusElevation: 5,
+        hoverElevation: 5,
+        highlightElevation: 5,
+        splashColor: Colors.white.withOpacity(0.1),
+        hoverColor: Colors.white,
+        backgroundColor: const Color(0xFFFEBB6C),
+        child: const Icon(Icons.edit),
+        onPressed: () {},
+      ),
       appBar: AppBar(
         centerTitle: true,
         iconTheme: const IconThemeData(color: Color(0xFF667080), size: 30),
         title: const Text(
           '고양이 계시판',
-          style: TextStyle(color: Color(0xFF667080)),
+          style: TextStyle(color: Color(0xFF667080), fontSize: 20),
         ),
         actions: [
           IconButton(
@@ -29,7 +40,7 @@ class CommonBoard extends GetView<CommonBoardController> {
             onPressed: () {},
           )
         ],
-        elevation: 1.5,
+        elevation: 3,
         backgroundColor: Color(0xFFFCFCFC),
       ),
       body: Container(),
